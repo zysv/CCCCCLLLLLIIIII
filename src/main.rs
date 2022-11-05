995cdfacee3896af91c2447da6eaeceacd1ef681
ZySvC <109306630+zysv@users.noreply.github.com>
2022-11-05 18:20:11 +0100

    Update main.rs
    Better argument support

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,16 @@ impl DiscardUntil for Stdin {
         Ok(())
     }
 }
-
+struct AppConfig {
+    pub target_url: Box<str>,
+    pub input_file: Box<str>,
+    pub output_file: Box<str>,
+    pub threads: usize,
+    pub proxies_file: Box<str>,
+    pub log_file: Box<str>,
+    pub delay: u64,
+    pub timeout: u64,
+}
 
 #[tokio::main]
 async fn main() {
@@ -50,10 +59,20 @@ async fn main() {
         .author("by ZySvC")
         .about("helllllllllooooooooooooooo")
         .arg(
+            Arg::new("target_url")
+                .short('t')
+                .long("target")
+                .value_name("target_url")
+                .help("Target ip or url for the timeout tests")
+                .default_value("https://ifconfig.me")
+                .num_args(1)
+        )
+        .arg(
             Arg::new("input_file")
                 .short('i')
                 .long("input")
-                .help("file containing your input list")
+                .value_name("input_file")
+                .help("File containing your input list")
                 .action(ArgAction::Set)
                 .num_args(1)
         )
@@ -61,51 +80,71 @@ async fn main() {
             Arg::new("output_file")
                 .short('o')
                 .long("output")
-                .help("file to write all valid parts of the list to")
+                .value_name("output_file")
+                .help("File to write all valid parts of the list to")
+                .default_value("valid_proxies.txt")
                 .action(ArgAction::Set)
                 .num_args(1)
         )
         .arg(
-            Arg::new("proxies")
+            Arg::new("threads")
+                .short('T')
+                .long("threads")
+                .value_name("threads")
+                .help("Number of threads to spawn")
+                .action(ArgAction::Set)
+                .num_args(1)
+        )
+        .arg(
+            Arg::new("proxy_file")
                 .short('p')
                 .long("proxies")
-                .value_name("proxyfile")
-                .help("file containing a line-separated list of proxies")
+                .value_name("proxy_file")
+                .help("File containing a line-separated list of proxies")
                 .action(ArgAction::Set)
                 .num_args(1),
         )
-        .arg(
-            Arg::new("jobs")
-                .short('j')
-                .long("jobs")
-                .value_name("# jobs")
-                .help("number of threads to spawn (defaults to # cpus available)")
-                .action(ArgAction::Set)
-                .num_args(1)
-        )
-        .arg(
-            Arg::new("verbose")
-                .short('v')
-                .long("verbose")
-                .help("enables verbose logging")
-        )
+        // .arg(
+        //     Arg::new("verbose")
+        //         .short('v')
+        //         .long("verbose")
+        //         .value_name("verbose")
+        //         .help("enables verbose logging")
+        // )
         .arg(
             Arg::new("log")
                 .short('l')
                 .long("log")
-                .value_name("log file")
+                .value_name("log_file")
                 .help("file to output logs to")
                 .action(ArgAction::Set)
                 .num_args(1)
         )
         .get_matches();
-
+    // let cfg = Command::new(AppConfig {
+    //     target_url: matches.value_of("target").unwrap().into(),
+    //     input: matches.value_of("input").unwrap().into(),
+    //     output: matches.value_of("output").unwrap().into(),
+    //     delay: 1000
+    //         / matches
+    //             .value_of("cons_per_sec")
+    //             .map(|x| x.parse().expect("Invalid amount of connections"))
+    //             .unwrap_or(30),
+    //     cores: matches
+    //         .value_of("cores")
+    //         .map(|x| x.parse().expect("Invalid amount of cores"))
+    //         .unwrap_or_else(num_cpus::get),
+    //     timeout: matches
+    //         .value_of("timeout")
+    //         .map(|x| x.parse().expect("Invalid timeout"))
+    //         .unwrap_or(5),
+    // })
     println!("{}", format!("    [{}] Press [{}] to continue", "+".bright_blue(), "ENTER".white().bold()).white());
 
     // wait for newline input
     io::stdin().discard_until_newline().unwrap();
 
-    while true {
-        log::info!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    }
+    // while true {
+    //     log::info!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    // }
 }
