c075452d806c24f2dcc86108eff262d93c26a120
ZySvC <109306630+zysv@users.noreply.github.com>
2022-11-05 22:45:15 +0100

    Configuration menu
    Arguments 'validation'
    Removed AppConfig
    Added repetitive print
    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 use std::io;
 use std::io::{Stdin, BufRead};
 use colored::*;
-use clap::{Command, Arg, ArgAction};
+use clap::{Command, Arg};
+// use terminal::{Action, Clear};
 
 // https://stackoverflow.com/questions/39351453/is-there-a-standard-way-of-discarding-input-in-rust
 pub trait DiscardUntil {
@@ -21,16 +22,25 @@ impl DiscardUntil for Stdin {
         Ok(())
     }
 }
-struct AppConfig {
-    pub target_url: Box<str>,
-    pub input_file: Box<str>,
-    pub output_file: Box<str>,
-    pub threads: usize,
-    pub proxies_file: Box<str>,
-    pub log_file: Box<str>,
-    pub delay: u64,
-    pub timeout: u64,
+
+fn rep_print(input: char, length: usize) -> String {
+    let mut cnstrcd = String::new();
+    for _ in 1..length {
+        // println!("{}.) '{}'", i,input);
+        cnstrcd.push(input);
+    }
+    return cnstrcd;
 }
+// struct AppConfig {
+//     pub target_url: Box<str>,
+//     pub input_file: Box<str>,
+//     pub output_file: Box<str>,
+//     pub threads: usize,
+//     pub proxies_file: Box<str>,
+//     pub log_file: Box<str>,
+//     pub delay: u64,
+//     pub timeout: u64,
+// }
 
 #[tokio::main]
 async fn main() {
@@ -45,21 +55,25 @@ async fn main() {
         ::log::set_max_level(log::LevelFilter::Debug);
     }
     
+    // clear terminal ; not recommended as of rn
+    // let terminal = terminal::stdout();
+    // terminal.act(Action::ClearTerminal(Clear::All)).expect("couldn't clear terminal");
+
     // welcome screen
     let cli_txt = format!("
-    _________ _________ _________  ___     ___     ___     ___ ___ ___
-    \\_   ___ \\\\_   ___ \\\\_   ___ \\|   |   |   |   |   |   |   |   |   |  -  {}{}{}
-    /    \\  \\//    \\  \\//    \\  \\/|   |   |   |   |   |   |   |   |   |
-    \\     \\____     \\____     \\____   |___|   |___|   |___|   |   |   |
-     \\______  /\\______  /\\______  /______ \\______ \\______ \\___|___|___|
-            \\/        \\/        \\/       \\/      \\/      \\/            
-    ", "github".bright_black(), "/".white(), "zysv".bright_blue());
+  _________ _________ _________  ___     ___     ___     ___ ___ ___
+  \\_   ___ \\\\_   ___ \\\\_   ___ \\|   |   |   |   |   |   |   |   |   |  -  {}{}{}
+  /    \\  \\//    \\  \\//    \\  \\/|   |   |   |   |   |   |   |   |   |
+  \\     \\____     \\____     \\____   |___|   |___|   |___|   |   |   |
+   \\______  /\\______  /\\______  /______ \\______ \\______ \\___|___|___|
+            \\/        \\/        \\/       \\/      \\/      \\/            ", 
+    "github".bright_black(), "/".white(), "zysv".bright_blue());
     
     // print welcome screen
     println!("{}", cli_txt.bright_red());
 
     let matches = Command::new("cccccllllliiiii")
-        .version("0.1.7")
+        .version("0.1.9")
         .author("by ZySvC")
         .about("helllllllllooooooooooooooo")
         .arg(
@@ -69,7 +83,6 @@ async fn main() {
                 .value_name("target_url")
                 .help("Target ip or url for the timeout tests")
                 .default_value("https://ifconfig.me")
-                .num_args(1)
         )
         .arg(
             Arg::new("input_file")
@@ -77,8 +90,6 @@ async fn main() {
                 .long("input")
                 .value_name("input_file")
                 .help("File containing your input list")
-                .action(ArgAction::Set)
-                .num_args(1)
         )
         .arg(
             Arg::new("output_file")
@@ -87,8 +98,6 @@ async fn main() {
                 .value_name("output_file")
                 .help("File to write all valid parts of the list to")
                 .default_value("valid_proxies.txt")
-                .action(ArgAction::Set)
-                .num_args(1)
         )
         .arg(
             Arg::new("threads")
@@ -96,8 +105,6 @@ async fn main() {
                 .long("threads")
                 .value_name("threads")
                 .help("Number of threads to spawn")
-                .action(ArgAction::Set)
-                .num_args(1)
         )
         // .arg(
         //     Arg::new("verbose")
@@ -106,35 +113,33 @@ async fn main() {
         //         .value_name("verbose")
         //         .help("enables verbose logging")
         // )
-        .arg(
-            Arg::new("log")
-                .short('l')
-                .long("log")
-                .value_name("log_file")
-                .help("file to output logs to")
-                .action(ArgAction::Set)
-                .num_args(1)
-        )
         .get_matches();
-    // let cfg = Command::new(AppConfig {
-    //     target_url: matches.value_of("target").unwrap().into(),
-    //     input: matches.value_of("input").unwrap().into(),
-    //     output: matches.value_of("output").unwrap().into(),
-    //     delay: 1000
-    //         / matches
-    //             .value_of("cons_per_sec")
-    //             .map(|x| x.parse().expect("Invalid amount of connections"))
-    //             .unwrap_or(30),
-    //     cores: matches
-    //         .value_of("cores")
-    //         .map(|x| x.parse().expect("Invalid amount of cores"))
-    //         .unwrap_or_else(num_cpus::get),
-    //     timeout: matches
-    //         .value_of("timeout")
-    //         .map(|x| x.parse().expect("Invalid timeout"))
-    //         .unwrap_or(5),
-    // })
-    println!("{}", format!("    [{}] Press [{}] to continue", "+".bright_blue(), "ENTER".white().bold()).white());
+
+    // TODO: Argument validation (colorful?)
+    
+    // validate required arguments for their existence, if not panic
+    let target_url = matches.get_one::<String>("target_url").expect("failed to get target");
+    let input_file = matches.get_one::<String>("input_file").expect("failed to get input_file");
+    let output_file = matches.get_one::<String>("output_file").expect("failed to get output_file");
+    let threads = matches.get_one::<String>("threads").expect("failed to get threads");
+
+    let cfgtxt = format!("
+  .-- [ Loaded Configuration ] ------------------------------------.
+  |  Target         -  {}{}|
+  |  Input File     -  {}{}|
+  |  Output File    -  {}{}|
+  |  Threads        -  {}{}|
+  '----------------------------------------------------------------'",
+    target_url.green(), rep_print(' ', 45 - target_url.chars().count()),
+    input_file.green(), rep_print(' ', 45 - input_file.chars().count()),
+    output_file.green(), rep_print(' ', 45 - output_file.chars().count()),
+    threads.green(), rep_print(' ', 45 - threads.chars().count()),
+    );
+    
+    // print configuration
+    println!("{}\n", cfgtxt);
+
+    println!("{}", format!("  [{}] Press [{}] to continue", "+".bright_blue(), "ENTER".white().bold()).white());
 
     // wait for newline input
     io::stdin().discard_until_newline().unwrap();
