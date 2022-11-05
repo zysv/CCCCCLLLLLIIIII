c261bf8b9616c2c0f8fb15b7fe5bc726785c4802
ZySvC <109306630+zysv@users.noreply.github.com>
2022-11-05 17:39:46 +0100

    Added argument support

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 use std::io;
 use std::io::{Stdin, BufRead};
 use colored::*;
+use clap::{Command, Arg, ArgAction};
+
 // https://stackoverflow.com/questions/39351453/is-there-a-standard-way-of-discarding-input-in-rust
 pub trait DiscardUntil {
     fn discard_until_newline(&mut self) -> Result<(), io::Error>;
@@ -40,8 +42,63 @@ async fn main() {
      \\______  /\\______  /\\______  /______ \\______ \\______ \\___|___|___|
             \\/        \\/        \\/       \\/      \\/      \\/            
     ", "github".bright_black(), "/".white(), "zysv".bright_blue());
-
+    
     println!("{}", cli_txt.bright_red());
+
+    let matches = Command::new("cccccllllliiiii")
+        .version("0.1.7")
+        .author("by ZySvC")
+        .about("helllllllllooooooooooooooo")
+        .arg(
+            Arg::new("input_file")
+                .short('i')
+                .long("input")
+                .help("file containing your input list")
+                .action(ArgAction::Set)
+                .num_args(1)
+        )
+        .arg(
+            Arg::new("output_file")
+                .short('o')
+                .long("output")
+                .help("file to write all valid parts of the list to")
+                .action(ArgAction::Set)
+                .num_args(1)
+        )
+        .arg(
+            Arg::new("proxies")
+                .short('p')
+                .long("proxies")
+                .value_name("proxyfile")
+                .help("file containing a line-separated list of proxies")
+                .action(ArgAction::Set)
+                .num_args(1),
+        )
+        .arg(
+            Arg::new("jobs")
+                .short('j')
+                .long("jobs")
+                .value_name("# jobs")
+                .help("number of threads to spawn (defaults to # cpus available)")
+                .action(ArgAction::Set)
+                .num_args(1)
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .help("enables verbose logging")
+        )
+        .arg(
+            Arg::new("log")
+                .short('l')
+                .long("log")
+                .value_name("log file")
+                .help("file to output logs to")
+                .action(ArgAction::Set)
+                .num_args(1)
+        )
+        .get_matches();
 
     println!("{}", format!("    [{}] Press [{}] to continue", "+".bright_blue(), "ENTER".white().bold()).white());
 
