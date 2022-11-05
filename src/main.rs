ccb0ccfdcb64eb6200833046206cfc8c96b86c88
ZySvC <109306630+zysv@users.noreply.github.com>
2022-11-05 01:50:18 +0100

    Added SimpleLogger crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
+    #[cfg(debug_assertions)]
+    {
+        simple_logger::SimpleLogger::new()
+            .with_threads(true)
+            .with_colors(true)
+            .init()
+            .unwrap();
+        ::log::set_max_level(log::LevelFilter::Debug);
+    }
+
     while true {
-        print!("A")
+        log::info!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 }
